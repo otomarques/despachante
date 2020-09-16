a0c75adc18e5cff05f3a7a72488e3e31a353983f
otomarques <otomarques@gmail.com>
2020-09-16 17:50:39 -0300

    criação do projeto

---
+++ despachante/src/test/java/com/oto/despachante/DespachanteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oto.despachante;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DespachanteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
