004d9c7df6c29c12cdacaf61a80f1eefea4c8451
pauroenriki <pauroenriki@gmail.com>
2021-04-05 18:36:18 -0300

    Implementando Security

--- despachante/src/main/java/com/oto/despachante/exception/ExceptionConfig.java
+++ despachante/src/main/java/com/oto/despachante/exception/ExceptionConfig.java
@@ -1,7 +1,9 @@
 package com.oto.despachante.exception;
 
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -10,7 +12,20 @@ public class ExceptionConfig {
 	@ExceptionHandler({
 		EmptyResultDataAccessException.class
 	})
-	public ResponseEntity errorNotFound(Exception e) {
+	public ResponseEntity errorNotFound() {
 		return ResponseEntity.notFound().build();
+	}
+	@ExceptionHandler({
+		AccessDeniedException.class
+	})
+	public ResponseEntity accessDenied() {
+		return ResponseEntity.status(HttpStatus.FORBIDDEN).body(new Error("Acesso Negado"));
+	}
+}
+
+class Error{
+	public String error;
+	public Error(String error) {
+		this.error = error;
 	}
 }
