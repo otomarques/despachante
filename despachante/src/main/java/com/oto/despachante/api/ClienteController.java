155aa3867438b5af2471b2785e4ea03ca98cfa40
pauroenriki <pauroenriki@gmail.com>
2021-05-17 16:34:36 -0300

    removendo segurity

--- despachante/src/main/java/com/oto/despachante/api/ClienteController.java
+++ despachante/src/main/java/com/oto/despachante/api/ClienteController.java
@@ -2,12 +2,9 @@ package com.oto.despachante.api;
 
 import java.net.URI;
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.annotation.Secured;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
--- despachante/src/main/java/com/oto/despachante/exception/ExceptionConfig.java
+++ despachante/src/main/java/com/oto/despachante/exception/ExceptionConfig.java
@@ -3,7 +3,6 @@ package com.oto.despachante.exception;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -15,9 +14,6 @@ public class ExceptionConfig {
 	public ResponseEntity errorNotFound() {
 		return ResponseEntity.notFound().build();
 	}
-	@ExceptionHandler({
-		AccessDeniedException.class
-	})
 	public ResponseEntity accessDenied() {
 		return ResponseEntity.status(HttpStatus.FORBIDDEN).body(new Error("Acesso Negado"));
 	}
